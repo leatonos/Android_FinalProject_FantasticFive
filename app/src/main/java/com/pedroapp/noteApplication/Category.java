b8feccad69486731ec21f2444eccceff410572e0
Pedrofmattosb <pedrofmattosb@gmail.com>
2021-06-14 20:48:22 -0300

    New update
    Now the category title appears on the screen when you click there

--- app/src/main/java/com/pedroapp/noteApplication/Category.java
+++ app/src/main/java/com/pedroapp/noteApplication/Category.java
@@ -4,13 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.pedroapp.noteApplication.util.CategoryChosen;
 
 public class Category extends AppCompatActivity {
+
+    TextView pageTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        pageTitle = findViewById(R.id.categoryTitle);
+        pageTitle.setText(CategoryChosen.chosenCategory);
+
     }
 
     public void goToAddNote(View view) {
